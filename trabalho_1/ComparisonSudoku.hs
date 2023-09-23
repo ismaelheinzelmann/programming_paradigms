246d1a90230635300f8ca09212a959596daba6c8
ismael <ismaelheinzelmann@gmail.com>
2023-09-23 17:19:47 -0300

    limpando e organizando

--- trabalho_1/ComparisonSudoku.hs
+++ trabalho_1/ComparisonSudoku.hs
@@ -1,188 +1,194 @@
-
 module ComparisonSudoku where
-import Data.List ( nub, (\\) )
-import Control.Monad
-import Control.Monad.State
+
+import Data.List (nub, (\\))
 import Data.Maybe
 import Debug.Trace
 
 type Board = [[Int]]
+
 data Comparison = Less | Greater | None deriving (Eq, Show)
+
 type ComparisonRows = [[Comparison]]
+
 type ComparisonColumns = [[Comparison]]
 
--- Sudoku de Comparação é um sudoku com comparações entre os números
--- Linhas e colunas de comparação são matrizes de comparações
--- Cada linha e coluna de comparação terá 6 elementos, e terão 9 linhas/colunas de comparação
--- Cada elemento é uma comparação entre dois números
--- Cada comparação é um Less ou um Greater
-
---tested
+-- Get do comparador de cima dado uma cordenada i j
 getComparatorUp :: ComparisonColumns -> Int -> Int -> Comparison
 getComparatorUp comparisionColumns row col
-    | row `mod` 3 == 0 = None
-    | otherwise = comparisionColumns !! col !! (row-1)
---tested
+  | row `mod` 3 == 0 = None
+  | otherwise = comparisionColumns !! col !! (row - 1)
+
+-- Get do comparador de baixo dado uma cordenada i j
 getComparatorDown :: ComparisonColumns -> Int -> Int -> Comparison
 getComparatorDown comparisionColumns row col
-    | row `mod` 3 == 2 = None
-    | otherwise = comparisionColumns !! col !! row
+  | row `mod` 3 == 2 = None
+  | otherwise = comparisionColumns !! col !! row
 
---tested
+-- Get do comparador da esquerda dado uma cordenada i j
 getComparatorLeft :: ComparisonRows -> Int -> Int -> Comparison
 getComparatorLeft comparisionRows row col
-    | col `mod` 3 == 0 = None
-    | otherwise = comparisionRows !! row !! (col-1)
+  | col `mod` 3 == 0 = None
+  | otherwise = comparisionRows !! row !! (col - 1)
 
---tested
+-- Get do comparador da direita dado uma cordenada i j
 getComparatorRight :: ComparisonRows -> Int -> Int -> Comparison
 getComparatorRight comparisionRows row col
-    | col `mod` 3 == 2 = None
-    | otherwise = comparisionRows !! row !! col
+  | col `mod` 3 == 2 = None
+  | otherwise = comparisionRows !! row !! col
 
+-- Retorna o valor da celula na linha e coluna
 getCell :: Board -> Int -> Int -> Int
 getCell board row col
-    | row < 0 = 0
-    | row > 8 = 0
-    | col < 0 = 0
-    | col > 8 = 0
-    | otherwise = board !! row !! col
+  | row < 0 = 0
+  | row > 8 = 0
+  | col < 0 = 0
+  | col > 8 = 0
+  | otherwise = board !! row !! col
 
--- Linha, Coluna, Valor
+-- Verifica se a linha é valida (não tem valores repetidos até o momento)
 validRow :: Board -> Int -> Bool
 validRow board rowNumber = do
-    --filter row to remove 0s
-    let row = filter (/= 0) (getRow board rowNumber)
-    length (nub row) == length row
+  -- filter row to remove 0s
+  let row = filter (/= 0) (getRow board rowNumber)
+  length (nub row) == length row
 
+-- Verifica se a célula é valida (se os comparadores dos vizinhos são satisfeitos)
 validCellComparisions :: Board -> ComparisonRows -> ComparisonColumns -> Int -> Int -> Bool
 validCellComparisions board comparisionRows comparisionColumns row col =
-    let comparatorUp = getComparatorUp comparisionColumns row col
-        comparatorDown = getComparatorDown comparisionColumns row col
-        comparatorLeft = getComparatorLeft comparisionRows row col
-        comparatorRight = getComparatorRight comparisionRows row col
-        left = getCell board row (col - 1)
-        right = getCell board row (col + 1)
-        up = getCell board (row - 1) col
-        down = getCell board (row + 1) col
-        value = board !! row !! col
-        validUp = case comparatorUp of
-            None -> True
-            Less -> case up of
-                0 -> True
-                otherwise -> up < value
-            Greater -> case up of
-                0 -> True
-                otherwise -> up > value
-        validDown = case comparatorDown of
-            None -> True
-            Less -> case down of
-                0 -> True
-                otherwise -> down > value
-            Greater -> case down of
-                0 -> True
-                otherwise -> down < value
-        validLeft = case comparatorLeft of
-            None -> True
-            Less -> case left of
-                0 -> True
-                otherwise -> left < value
-            Greater -> case left of
-                0 -> True
-                otherwise -> left > value
-        validRight = case comparatorRight of
-            None -> True
-            Less -> case right of
-                0 -> True
-                otherwise -> right > value
-            Greater -> case right of
-                0 -> True
-                otherwise -> right < value
-    in validUp && validDown && validLeft && validRight
+  let comparatorUp = getComparatorUp comparisionColumns row col
+      comparatorDown = getComparatorDown comparisionColumns row col
+      comparatorLeft = getComparatorLeft comparisionRows row col
+      comparatorRight = getComparatorRight comparisionRows row col
+      left = getCell board row (col - 1)
+      right = getCell board row (col + 1)
+      up = getCell board (row - 1) col
+      down = getCell board (row + 1) col
+      value = board !! row !! col
+      validUp = case comparatorUp of
+        None -> True
+        Less -> case up of
+          0 -> True
+          otherwise -> up < value
+        Greater -> case up of
+          0 -> True
+          otherwise -> up > value
+      validDown = case comparatorDown of
+        None -> True
+        Less -> case down of
+          0 -> True
+          otherwise -> down > value
+        Greater -> case down of
+          0 -> True
+          otherwise -> down < value
+      validLeft = case comparatorLeft of
+        None -> True
+        Less -> case left of
+          0 -> True
+          otherwise -> left < value
+        Greater -> case left of
+          0 -> True
+          otherwise -> left > value
+      validRight = case comparatorRight of
+        None -> True
+        Less -> case right of
+          0 -> True
+          otherwise -> right > value
+        Greater -> case right of
+          0 -> True
+          otherwise -> right < value
+   in validUp && validDown && validLeft && validRight
 
---verify if have any repeated values in the column
+-- Verifica se a coluna é valida (não tem valores repetidos até o momento)
 validColumn :: Board -> Int -> Bool
 validColumn board col = do
-    let column = filter (/= 0) (getColumn board col)
-    length (nub column) == length column
+  let column = filter (/= 0) (getColumn board col)
+  length (nub column) == length column
 
+-- Verifica se o bloco é valido (não tem valores repetidos até o momento)
 validBlock :: Board -> Int -> Int -> Bool
 validBlock board row col =
-    let block = getBlock board row col
-        blockWithoutZeros = filter (/= 0) block
-    in length (nub blockWithoutZeros) == length blockWithoutZeros
+  let block = getBlock board row col
+      blockWithoutZeros = filter (/= 0) block
+   in length (nub blockWithoutZeros) == length blockWithoutZeros
 
-
+-- Verifica se o board é valido (colunas válidas, linhas válidas, blocos válidos e todas as células respeitam os comparadores)
 validBoard :: Board -> ComparisonRows -> ComparisonColumns -> Bool
 validBoard board comparisionRows comparisionColumns =
-    let validRows = all (validRow board) [0..8]
-        validColumns = all (validColumn board) [0..8]
-        validBlocks = all (\row -> all (validBlock board row) [0,3,6]) [0,3,6]
-        validPlaces = all (\row -> all (validCellComparisions board comparisionRows comparisionColumns row) [0..8]) [0..8]
-    in validRows && validColumns && validBlocks && validPlaces
+  let validRows = all (validRow board) [0 .. 8]
+      validColumns = all (validColumn board) [0 .. 8]
+      validBlocks = all (\row -> all (validBlock board row) [0, 3, 6]) [0, 3, 6]
+      validPlaces = all (\row -> all (validCellComparisions board comparisionRows comparisionColumns row) [0 .. 8]) [0 .. 8]
+   in validRows && validColumns && validBlocks && validPlaces
 
---Verify if the value is valid to be placed in the next empty cell on board
+-- Verifica se a inserção de um valor na linha e coluna é valida
 validInsert :: Board -> ComparisonRows -> ComparisonColumns -> Int -> Bool
 validInsert board comparisionRows comparisionColumns value =
-    let nextEmpty = firstEmpty board
-    in case nextEmpty of
+  let nextEmpty = firstEmpty board
+   in case nextEmpty of
         Nothing -> validBoard board comparisionRows comparisionColumns
         Just (row, col) -> do
-            let newBoard = insertFirstEmpty board value
-                validComparisions = validCellComparisions (insertFirstEmpty board value) comparisionRows comparisionColumns row col
-                validR = validRow newBoard row
-                validC = validColumn newBoard col
-                validB = validBlock newBoard row col
-            validComparisions && validR && validC && validB
+          let newBoard = insertFirstEmpty board value
+              validComparisions = validCellComparisions (insertFirstEmpty board value) comparisionRows comparisionColumns row col
+              validR = validRow newBoard row
+              validC = validColumn newBoard col
+              validB = validBlock newBoard row col
+          validComparisions && validR && validC && validB
 
+-- Altera o valor de uma celula na linha e coluna
 replace :: [[Int]] -> Int -> Int -> Int -> [[Int]]
 replace block row col value =
-    let (xs, y:ys) = splitAt row block
-        (zs, _:ws) = splitAt col y
-    in xs ++ [zs ++ [value] ++ ws] ++ ys
+  let (xs, y : ys) = splitAt row block
+      (zs, _ : ws) = splitAt col y
+   in xs ++ [zs ++ [value] ++ ws] ++ ys
 
+-- Verifica se o board está cheio
 isFull :: [[Int]] -> Bool
 isFull = not . any (elem 0)
 
---find first row col containing 0 in board, return Nothing if not found
+-- Retorna a próxima cordenada válida para inserção
 firstEmpty :: Board -> Maybe (Int, Int)
-firstEmpty board = listToMaybe [(row, col) | row <- [0..8], col <- [0..8], board !! row !! col == 0]
+firstEmpty board = listToMaybe [(row, col) | row <- [0 .. 8], col <- [0 .. 8], board !! row !! col == 0]
 
---Print board
+-- Print board
 printBoard :: Board -> IO ()
 printBoard = mapM_ print
 
---tested
+-- Retorna uma lista com o bloco da cordenada
 getBlock :: Board -> Int -> Int -> [Int]
 getBlock board row col =
-    let blockRow = row `div` 3
-        blockCol = col `div` 3
-        startRow = blockRow * 3
-        startCol = blockCol * 3
-    in [board !! row !! col | row <- [startRow..startRow + 2], col <- [startCol..startCol + 2]]
+  let blockRow = row `div` 3
+      blockCol = col `div` 3
+      startRow = blockRow * 3
+      startCol = blockCol * 3
+   in [board !! row !! col | row <- [startRow .. startRow + 2], col <- [startCol .. startCol + 2]]
 
+-- Retorna a  n linha
 getRow :: Board -> Int -> [Int]
 getRow board row = board !! row
 
+-- Retorna a n coluna
 getColumn :: Board -> Int -> [Int]
 getColumn board col = map (!! col) board
 
+-- Insere o valor no primeiro espaço vazio
 insertFirstEmpty :: Board -> Int -> Board
 insertFirstEmpty board value = do
-    let empty = firstEmpty board
-    case empty of
-        Nothing -> board
-        Just (row, col) -> replace board row col value
+  let empty = firstEmpty board
+  case empty of
+    Nothing -> board
+    Just (row, col) -> replace board row col value
 
-solveSudokuBacktracking :: Board -> ComparisonRows -> ComparisonColumns -> Maybe Board
-solveSudokuBacktracking board comparisionRows comparisionColumns =
-    if isFull board then do
-        traceM $ "Full" ++ show board
-        if validBoard board comparisionRows comparisionColumns
-            then do
-                Just board
-            else Nothing
+-- Utiliza recursão para resolver o sudoku
+solveSudokuRecursive :: Board -> ComparisonRows -> ComparisonColumns -> Maybe Board
+solveSudokuRecursive board comparisionRows comparisionColumns =
+  if isFull board
+    then do
+      if validBoard board comparisionRows comparisionColumns
+        then do
+          Just board
+        else Nothing
     else do
-        traceM $ "Not Full: " ++ show board
-        let validPlacements = filter (\x -> validInsert board comparisionRows comparisionColumns x) [1..9]
-        if null validPlacements then Nothing else listToMaybe $ mapMaybe (\value -> solveSudokuBacktracking (insertFirstEmpty board value) comparisionRows comparisionColumns) validPlacements
+      -- debug
+      -- traceM $ "Not Full: " ++ show board
+      let validPlacements = filter (\x -> validInsert board comparisionRows comparisionColumns x) [1 .. 9]
+      if null validPlacements then Nothing else listToMaybe $ mapMaybe (\value -> solveSudokuRecursive (insertFirstEmpty board value) comparisionRows comparisionColumns) validPlacements
--- trabalho_1/main.hs
+++ trabalho_1/main.hs
@@ -1,28 +1,55 @@
 import ComparisonSudoku
-import Text.XHtml (background)
 
 main :: IO ()
 main = do
-    let board :: Board = replicate 9 (replicate 9 0)
-    let comparisionRows :: ComparisonRows = [[Less, Greater, None,Less, Less,None, Greater, Greater],
-                                              [Less, Less, None, Less, Greater,None, Greater, Less],
-                                              [Less, Less,None, Less, Greater,None, Greater, Less],
-                                              [Greater, Greater,None, Greater, Less,None, Less, Greater],
-                                              [Greater, Less,None, Greater, Less,None, Greater, Greater],
-                                              [Less, Greater,None, Less, Greater,None, Greater, Less],
-                                              [Less, Greater,None, Greater, Greater,None, Less, Greater],
-                                              [Greater, Less,None, Greater, Less,None, Less, Less],
-                                              [Less, Greater,None, Less, Less,None, Less, Greater]]
-    let comparisionColumns :: ComparisonColumns = [[Greater, Less, None,Greater, Greater,None, Greater, Less],
-                                                    [Less, Less,None, Less, Less,None, Greater, Less],
-                                                    [Less, Less,None, Less, Less,None, Greater, Greater],
-                                                    [Greater, Less,None, Greater, Less,None, Less, Greater],
-                                                    [Greater, Greater,None, Greater, Less,None, Greater, Greater],
-                                                    [Greater, Greater,None, Greater, Greater,None, Greater, Less],
-                                                    [Less, Greater,None, Less, Greater,None, Less, Greater],
-                                                    [Greater, Greater,None, Greater, Greater,None, Less, Less],
-                                                    [Less, Greater,None, Less, Greater,None, Less, Greater]]
-    let solution :: Maybe Board = solveSudokuBacktracking board comparisionRows comparisionColumns
-    case solution of
-        Just board -> printBoard board
-        Nothing -> putStrLn "No solution found."
+  let board :: Board = replicate 9 (replicate 9 0)
+  --   let comparisionRows :: ComparisonRows =
+  --         [ [Less, Greater, None, Less, Less, None, Greater, Greater],
+  --           [Less, Less, None, Less, Greater, None, Greater, Less],
+  --           [Less, Less, None, Less, Greater, None, Greater, Less],
+  --           [Greater, Greater, None, Greater, Less, None, Less, Greater],
+  --           [Greater, Less, None, Greater, Less, None, Greater, Greater],
+  --           [Less, Greater, None, Less, Greater, None, Greater, Less],
+  --           [Less, Greater, None, Greater, Greater, None, Less, Greater],
+  --           [Greater, Less, None, Greater, Less, None, Less, Less],
+  --           [Less, Greater, None, Less, Less, None, Less, Greater]
+  --         ]
+  --   let comparisionColumns :: ComparisonColumns =
+  --         [ [Greater, Less, None, Greater, Greater, None, Greater, Less],
+  --           [Less, Less, None, Less, Less, None, Greater, Less],
+  --           [Less, Less, None, Less, Less, None, Greater, Greater],
+  --           [Greater, Less, None, Greater, Less, None, Less, Greater],
+  --           [Greater, Greater, None, Greater, Less, None, Greater, Greater],
+  --           [Greater, Greater, None, Greater, Greater, None, Greater, Less],
+  --           [Less, Greater, None, Less, Greater, None, Less, Greater],
+  --           [Greater, Greater, None, Greater, Greater, None, Less, Less],
+  --           [Less, Greater, None, Less, Greater, None, Less, Greater]
+  --         ]
+
+  --https://www.janko.at/Raetsel/Sudoku/Vergleich/012.a.htm
+  let comparisionRows :: ComparisonRows =
+        [ [Less, Greater, None, Greater, Less, None, Less, Less],
+          [Greater, Less, None, Less, Less, None, Greater, Less],
+          [Greater, Less, None, Less, Greater, None, Less, Greater],
+          [Less, Less, None, Greater, Less, None, Greater, Less],
+          [Less, Greater, None, Greater, Less, None, Greater, Greater],
+          [Greater, Greater, None, Greater, Less, None, Less, Greater],
+          [Greater, Less, None, Less, Greater, None, Less, Less],
+          [Greater, Less, None, Greater, Greater, None, Greater, Greater],
+          [Less, Greater, None, Less, Less, None, Less, Less]
+        ]
+  let comparisionColumns :: ComparisonColumns =
+        [ [Less, Less, None, Greater, Less, None, Less, Greater],
+          [Greater, Greater, None, Greater, Greater, None, Less, Less],
+          [Less, Greater, None, Greater, Less, None, Greater, Greater],
+          [Less, Less, None, Less, Less, None, Less, Greater],
+          [Less, Greater, None, Less, Less, None, Greater, Less],
+          [Less, Greater, None, Less, Greater, None, Greater, Less],
+          [Greater, Less, None, Less, Greater, None, Less, Greater],
+          [Greater, Less, None, Less, Less, None, Greater, Less],
+          [Greater, Greater, None, Greater, Greater, None, Greater, Less]
+        ]
+  let solution :: Maybe Board = solveSudokuRecursive board comparisionRows comparisionColumns
+  case solution of
+    Just board -> printBoard board
+    Nothing -> putStrLn "No solution found."
